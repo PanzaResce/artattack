a96e287c7b77c6f135dab72ddad35a72c72745e2
panarellimarco99 <panarellimarco99@gmail.com>
2020-05-11 18:41:56 +0200

    Added merge panel
    Start button not working yet because a method to initialize classes based on the file name (crypt, zip, type, ...) is nedeed

--- src/main/java/GUI/MergePanel.java
+++ src/main/java/GUI/MergePanel.java
@@ -1,9 +1,20 @@
 package GUI;
 
 import java.awt.Color;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.swing.JFileChooser;
+
+import com.artattack.DivisionHandler;
+import com.artattack.FileDiv;
+import com.artattack.PartDiv;
 
 public class MergePanel extends AppPanel{
-
+	
+	DivisionHandler mergeQueue = new DivisionHandler();
+	
 	public MergePanel(int w, int h) {
 		super(w, h);
 	}
@@ -12,11 +23,56 @@ public class MergePanel extends AppPanel{
 	}
 	
 	
+	@SuppressWarnings("unchecked")
 	@Override
-	protected void addElementToContainer() {
-		JobUI job = new JobUI();
+	protected void addElementToContainer(String fname) {
+		JobUI job = new JobUI(fname);
 		container.add(job);
+		addJob(job);
+		revalidate();
+	}
+	
+	@Override
+	protected void startBtnAction() {
+		for(int i = 0; i < jobQueue.size(); i++) {
+			/*System.out.print(getJob(i).getJobType().getSelectedItem());
+			System.out.print(getJob(i).toZip());
+			System.out.println(getJob(i).toCrypt());
+			*/
+			String fname = getJob(i).getFileName().getText();
+			
+			mergeQueue.addFile(fname);
+			
+			System.out.println("ADDED");
+		}
+		
+		try {
+			mergeQueue.merge();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		mergeQueue.clear();
+		clearContainer();
 
 	}
+	
+	@Override
+	protected void fileBtnAction() {
+		JFileChooser fc = new JFileChooser();
+		int returnVal = fc.showOpenDialog(this);
+		
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            File file = fc.getSelectedFile();
+        	addElementToContainer(file.getAbsolutePath());
+        }
 
+	}
+	
+	@Override
+	protected JobUI getJob(int index) {
+		return (JobUI)jobQueue.get(index);
+	}
+	
+	
 }
